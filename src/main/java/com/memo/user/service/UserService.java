513e3f778ec2c6b936a108bc69acdf9f9cb4421d
xxyeon <jykim9335@gmail.com>
2025-06-13 17:27:22 +0900

    fix: 로그아웃과 회원탈퇴 로직 수정 - 카카오 로그아웃 시에만 카카오 토큰 삭제 - 카카오 회원탈퇴시 리프레시 토큰 삭제와 블랙리스트 등록

--- src/main/java/com/memo/user/service/UserService.java
+++ src/main/java/com/memo/user/service/UserService.java
@@ -18,10 +18,9 @@ import com.memo.common.jwt.TokenProvider;
 import com.memo.common.util.CustomPasswordEncoder;
 import com.memo.common.util.EmailService;
 import com.memo.common.util.UtilString;
-import com.memo.storage.RefreshTokenRepository;
-import com.memo.storage.TokenBlackListRepository;
 import com.memo.storage.TokenRepository;
 import com.memo.user.DTO.UserRequestDto;
+import com.memo.user.entity.LoginType;
 import com.memo.user.entity.User;
 import com.memo.user.oauth.CustomOAuthService;
 import com.memo.user.oauth.kakao.KakaoApiClient;
@@ -87,16 +86,11 @@ public class UserService {
 		//엑세스토큰으로 요청
 		String token = request.getHeader(UtilString.AUTHORIZATION.value()); //서비스 토큰
 		String jwt = TokenProvider.resolveToken(token);
-		String accessToken = tokenRepository.findByKey("kakaoAccess;id" + user.getId());
+
 		switch(user.getLoginType()) {
 			case NATIVE -> logout(jwt);
-			case KAKAO -> kakaoApiClient.logout(accessToken, user);
+			case KAKAO -> kakaoApiClient.logout(user);
 		}
-		tokenRepository.deleteByKey("refresh;id" + user.getId());
-		//브라우저 토큰 만료 -> 쿠키, 헤더 토큰삭제(프론트 역할), 디비에서 삭제
-		tokenRepository.save("blackList;id" + user.getId(), token);
-		tokenRepository.deleteByKey("kakaoAccess;id" + user.getId());
-		tokenRepository.deleteByKey("kakaoRefresh;id" + user.getId());
 	}
 
 	@Transactional
@@ -105,8 +99,6 @@ public class UserService {
 		tokenRepository.deleteByKey("refresh;id" + stringId);
 		//브라우저 토큰 만료 -> 쿠키, 헤더 토큰삭제(프론트 역할), 디비에서 삭제
 		tokenRepository.save("blackList;id" + stringId, token);
-		tokenRepository.deleteByKey("kakaoAccess;id" + stringId);
-		tokenRepository.deleteByKey("kakaoRefresh;id" + stringId);
 		//리프레시 쿠키 삭제하도록 응답
 	}
 
@@ -186,20 +178,17 @@ public class UserService {
 		//카카오 로그인 사용자인지 확인
 		String token = request.getHeader(UtilString.AUTHORIZATION.value()); //서비스 토큰
 		String jwt = TokenProvider.resolveToken(token);
-		switch (user.getLoginType()) {
-			case KAKAO -> kakaoApiClient.logout(jwt); //카카오 르그아웃에서 쿠키 삭제, 블랙리스트 등록, 리스레시 토큰 삭제 진행하고 있음
-			case NATIVE -> deleteTokenById(user.getId(), jwt, response);
+		if (user.getLoginType() == LoginType.KAKAO) {
+			kakaoApiClient.logout(user); //카카오 르그아웃에서 쿠키 삭제, 블랙리스트 등록, 리스레시 토큰 삭제 진행하고 있음
 		}
+		deleteTokenById(user.getId(), jwt, response);
 		userRepository.softDeleteById(user.getId());
 	}
 
 	private void deleteTokenById(Long id, String jwt, HttpServletResponse response) {
-		//delete(Entity)와 deleteById(Long) 차이
-		// userRepository.deleteById(id);
-
 		//블랙리스트에 추가,토큰 비우기, 쿠키
-		tokenBlackListStore.save(new TokenBlackList(jwt));
+		tokenRepository.save("blackList;id"+id, jwt);
 		deleteCookie(response);
-		refreshTokenStore.deleteByUserId(id);
+		tokenRepository.deleteByKey("refresh;id" + id);
 	}
 }
