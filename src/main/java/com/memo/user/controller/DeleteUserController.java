60a09b0c76175083871e01410cad4193531443f3
xxyeon <jykim9335@gmail.com>
2025-06-13 17:33:55 +0900

    feat: 회원탈퇴 swagger 적용

--- src/main/java/com/memo/user/controller/DeleteUserController.java
+++ src/main/java/com/memo/user/controller/DeleteUserController.java
@@ -8,11 +8,14 @@ import com.memo.common.security.CustomUserDetails;
 import com.memo.user.entity.User;
 import com.memo.user.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "User API", description = "유저 도메인 API")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -20,6 +23,9 @@ public class DeleteUserController {
 
 	private final UserService userService;
 
+	@Operation(
+		summary = "회원탈퇴"
+	)
 	@DeleteMapping("/api/users")
 	public void deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails, HttpServletRequest request, HttpServletResponse response) {
 		//카카오 로그인 사용자인지 확인) {
